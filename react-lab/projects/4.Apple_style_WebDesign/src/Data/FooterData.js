bee9d1f74a02da412f7f0c14a780638760190bc8
Srushti <rindhesrushti2006@gmail.com>
2025-11-04 20:31:30 +0530

    uploaded web app task in my internship

---
+++ react-lab/projects/4.Apple_style_WebDesign/src/Data/FooterData.js
@@ -0,0 +1,15 @@
+const footerData = [{
+        title: 'Product',
+        name: ['Features', 'Pricing', 'Documentation', 'API Reference'],
+    },
+    {
+        title: 'Company',
+        name: ['About Us', 'Careers', 'Contact'],
+    },
+    {
+        title: 'Resources',
+        name: ['Blog', 'Help Center', 'Community'],
+    },
+];
+
+export default footerData;
